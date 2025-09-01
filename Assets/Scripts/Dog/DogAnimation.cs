7404a42ee7afcb59e783a3b3a9a6fac4c3982cfa
Roger Lozano <lozanofierroruu@gmail.com>
2025-09-01 08:09:05 +0200

    wip: adding the logical to animation running

--- Assets/Scripts/Dog/DogAnimation.cs
+++ Assets/Scripts/Dog/DogAnimation.cs
@@ -1,3 +1,4 @@
+using TOGETHER.Player;
 using UnityEngine;
 
 namespace TOGETHER.Assets.Scripts.Dog
@@ -11,6 +12,7 @@ namespace TOGETHER.Assets.Scripts.Dog
 
         private Animator m_animatorDog;
         private DogMovement m_dogMovement;
+        private PlayerMove m_player;
 
         #endregion
 
@@ -20,24 +22,45 @@ namespace TOGETHER.Assets.Scripts.Dog
         {
             m_animatorDog = GetComponent<Animator>();
             m_dogMovement = GetComponent<DogMovement>();
+            m_player = FindAnyObjectByType<PlayerMove>();
         }
 
         private void OnEnable()
         {
             m_dogMovement.OnDogisMoving += DogMove;
+            m_player.OnPlayerIsRunning += Run;
         }
 
         private void OnDisable()
         {
             m_dogMovement.OnDogisMoving -= DogMove;
+            m_player.OnPlayerIsRunning -= Run;
         }
 
         private void DogMove(bool value)
         {
             if (value)
+            {
+                m_animatorDog.SetBool("IsIdleing", false);
                 m_animatorDog.SetBool("IsWalking", true);
+                m_animatorDog.SetBool("IsRunning", false);
+            }
             else
+            {
+                m_animatorDog.SetBool("IsIdleing", true);
                 m_animatorDog.SetBool("IsWalking", false);
+                m_animatorDog.SetBool("IsRunning", false);
+            }
+        }
+
+        private void Run(bool value)
+        {
+            if (value)
+            {
+                m_animatorDog.SetBool("IsIdleing", false);
+                m_animatorDog.SetBool("IsWalking", false);
+                m_animatorDog.SetBool("IsRunning", true);
+            }
         }
 
         #endregion
