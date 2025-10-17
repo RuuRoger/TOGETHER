144fde4a6cd5219f3be68c6f8b7c0b6b58884804
Roger Lozano <lozanofierroruu@gmail.com>
2025-10-17 08:02:29 +0200

    feat: new materials

--- Assets/Scripts/Enemies/EnemyColliderShootDark.cs
+++ Assets/Scripts/Enemies/EnemyColliderShootDark.cs
@@ -4,7 +4,7 @@ namespace TOGETHER.Assets.Scripts.Enemies
 {
     public class EnemyColliderShootDark : MonoBehaviour
     {
-        private void OnTrigerEnter(Collider other)
+        private void OnTriggerEnter(Collider other)
         {
             if (other.CompareTag("Ice"))
             {
