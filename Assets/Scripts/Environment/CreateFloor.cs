c2569cb7a1dd18ef246fe60ed1e46b25f2f1c6d7
Roger Lozano <lozanofierroruu@gmail.com>
2025-10-24 22:33:26 +0200

    .

--- Assets/Scripts/Environment/CreateFloor.cs
+++ Assets/Scripts/Environment/CreateFloor.cs
@@ -4,6 +4,13 @@ namespace TOGETHER.Assets.Scripts.Environment
 {
     public class CreateFloor : MonoBehaviour
     {
-        
+        //Fields
+        [SerializeField] private GameObject m_floor;
+
+        private void Awake()
+        {
+            
+        }
+
     }
 }
