d4dcd7fa6596730dda64b4b96ae48a7a0c1fa7f6
Roger Lozano <lozanofierroruu@gmail.com>
2025-10-26 01:13:51 +0200

    feat: create the floor level

--- Assets/Scripts/Environment/CreateFloor.cs
+++ Assets/Scripts/Environment/CreateFloor.cs
@@ -1,15 +1,29 @@
 using UnityEngine;
 
-namespace TOGETHER.Assets.Scripts.Environment
+namespace Assets.Scripts.Environment
 {
     public class CreateFloor : MonoBehaviour
     {
-        //Fields
-        [SerializeField] private GameObject m_floor;
+        // =================================== FIELDS ===================================
+        [SerializeField] private GameObject m_floorPrefab;
 
-        private void Awake()
+        // =================================== PRIVATE METHODS ===================================
+
+        private void Start()
         {
-            
+            MakeFloor();
+        }
+
+        private void MakeFloor()
+        {
+            for (float k = 9f; k <= 29f; k++)
+            {
+                for (float i = 1f; i <= 29f; i += 2)
+                {
+                    Vector3 prefabPosition = new(i, 0f, k);
+                    GameObject prefabFloorInstantiate = Instantiate(m_floorPrefab, prefabPosition, Quaternion.identity);
+                }
+            }
         }
 
     }
