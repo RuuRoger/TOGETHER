b0a16e68f57ccbeba55377ecbb21740b73aa1fec
Roger Lozano <lozanofierroruu@gmail.com>
2025-11-02 17:37:07 +0100

    wip: script to control game. WI

---
+++ Assets/Scripts/Manager/GameManager.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+namespace Assets.Scripts.Manager
+{
+    public class GameManager : MonoBehaviour
+    {
+        // =================================== FIELDS ===================================
+        [SerializeField] private GameObject m_dog;
+        [SerializeField] private GameObject m_cellPrefab;
+
+    }
+}
