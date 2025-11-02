de9007dc6f92b4ee796c247beb209fb1b0775112
Roger Lozano <lozanofierroruu@gmail.com>
2025-11-02 17:23:32 +0100

    feat: atributes

--- Assets/Scripts/Cells/Cell.cs
+++ Assets/Scripts/Cells/Cell.cs
@@ -6,21 +6,41 @@ namespace Assets.Scripts.Cells
     {
         // =================================== FIELDS ===================================
         private Vector2 m_idCell;
+        private bool m_isAccesible;
+        private Renderer m_render;
+        private Color m_cellColor = new(0.4f, 0.7f, 0.02f, 1f);
 
         // =================================== PROPERTIES ===================================
         public Vector2 IDCell
         {
-            get
-            {
-                return m_idCell;
-            }
-            set
-            {
-                m_idCell = value;
-            }
+            get { return m_idCell; }
+            set { m_idCell = value; }
+        }
+
+        public bool IsAccesible
+        {
+            get { return m_isAccesible; }
+            set { m_isAccesible = value; }
+        }
+
+        public Color CellColor
+        {
+            get { return m_cellColor; }
+            set { m_cellColor = value; }
         }
 
         // =================================== PRIVATE METHODS ===================================
+        private void Awake()
+        {
+            m_render = GetComponent<Renderer>();
+            m_isAccesible = false;
+        }
+
+        private void Start()
+        {
+            m_render.material.color = m_cellColor;
+        }
+
         private void OnMouseDown()
         {
             Debug.Log(IDCell);
