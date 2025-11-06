0cd8c68880da2ed4fc92710dfa1dfb020363838f
Roger Lozano <lozanofierroruu@gmail.com>
2025-11-06 20:39:08 +0100

    wip: script to control aniamtion

---
+++ Assets/Scripts/Animations/BasePlayerAnimationController.cs
@@ -0,0 +1,13 @@
+using System;
+using UnityEngine;
+using Assets.Scripts.Commons;
+
+namespace Assets.Scripts.Animations
+{
+    public class BasePlayerAnimationController : MonoBehaviour
+    {
+        // =================================== FIELDS ===================================
+        [SerializeField] private PlayerState m_playerState;
+
+    }
+}
