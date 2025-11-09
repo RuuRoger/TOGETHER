f29586b3adca7411a5dd65e6ec662916779eb053
Roger Lozano <lozanofierroruu@gmail.com>
2025-11-09 15:19:11 +0100

    feat. singleton

--- Assets/Scripts/Managers/CellManager.cs
+++ Assets/Scripts/Managers/CellManager.cs
@@ -1,77 +1,31 @@
 using System;
+using System.Collections.Generic;
 using UnityEngine;
-using Assets.Scripts.Players;
-using Assets.Scripts.Cells;
 
 namespace Assets.Scripts.Managers
 {
     public class CellManager : MonoBehaviour
     {
         // ================================================== FIELDS ==================================================
-        private BasePlayer m_basPlayer;
+        private static CellManager m_instance;
 
-        // ================================================== EVENTS ==================================================
-        public event Action<Vector2> OnStartTurn; //Goes to 'Cell.cs'
-
-        // ================================================== EVENT SUSCRIPTIONS ==================================================
-
-        ///<Summary>
-        /// Unsuscribe if is necessary
-        ///</Summary
-        private void OnDisable()
+        // ================================================== PROPERTIES ==================================================
+        public static CellManager Instance
         {
-            if (m_basPlayer != null)
-            {
-                m_basPlayer.OnIDCellPlayer -= HandlerAccesibleCells;
-            }
-        }
-
-        // ================================================== PUBLIC METHODS ==================================================
-
-        ///<Summary>
-        /// Suscribe the event checking if is possible
-        ///</Summary
-        public void SubscribeToBasePlayerEvents(BasePlayer player)
-        {
-
-            if (player != null)
-            {
-                m_basPlayer = player;
-                m_basPlayer.OnIDCellPlayer += HandlerAccesibleCells;
-            }
-        }
-
-        ///<Summary>
-        /// Call a method
-        /// Accesible public method to resstart the cell's colours
-        /// This happens when the player touched some cell to cancell character selection
-        ///</Summary
-        public void ResetStatus()
-        {
-            ChangeBasePlayerSelection();
-
-            foreach (Cell cell in FindObjectsByType<Cell>(FindObjectsSortMode.None))
-            {
-                cell.ResetColor();
-            }
+            get { return m_instance; }
+            set { m_instance = value; }
         }
 
         // ================================================== PRIVATE METHODS ==================================================
-
-        ///<Summary>
-        /// Notify to Calculate if a cell is accesible or not
-        ///</Summary
-        private void HandlerAccesibleCells(Vector2 playerIdCell)
+        private void Awake()
         {
-            OnStartTurn?.Invoke(playerIdCell);
-        }
+            if (Instance != null && Instance != this)
+            {
+                Destroy(gameObject);
+                return;
+            }
 
-        ///<Summary>
-        /// Change the bool BasePlayer.cs propertie to false
-        ///</Summary
-        private void ChangeBasePlayerSelection()
-        {
-            m_basPlayer.IsSelected = false;
+            Instance = this;
         }
     }
 }
