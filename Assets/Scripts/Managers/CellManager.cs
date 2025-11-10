22234b469db44e5fe5b6fc6090ad64cf5385b11c
Roger Lozano <lozanofierroruu@gmail.com>
2025-11-10 07:51:37 +0100

    feat: a method to add cells in the list and then a method to iterate to trhow the id dog for evaluate if is necessary paint another colour

--- Assets/Scripts/Managers/CellManager.cs
+++ Assets/Scripts/Managers/CellManager.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using Assets.Scripts.Cells;
 using UnityEngine;
 
 namespace Assets.Scripts.Managers
@@ -8,12 +9,30 @@ namespace Assets.Scripts.Managers
     {
         // ================================================== FIELDS ==================================================
         private static CellManager m_instance;
+        private List<Cell> m_cells = new List<Cell>();
 
         // ================================================== PROPERTIES ==================================================
         public static CellManager Instance
         {
             get { return m_instance; }
             set { m_instance = value; }
+        }
+
+        // ================================================== PUBLIC METHODS ==================================================
+        public void AddCell(Cell cell)
+        {
+            m_cells.Add(cell);
+        }
+
+        ///<summary>
+        /// Iterate each cell to dog's IDcell
+        ///</summary>
+        public void NotifyChangeColorAccesibleCells(Vector2 idDogCell)
+        {
+            foreach (var cell in m_cells)
+            {
+                cell.ChangeColorAccesibleCells(idDogCell);
+            }
         }
 
         // ================================================== PRIVATE METHODS ==================================================
