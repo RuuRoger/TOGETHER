9e715f2a0594a55c8e6fb4e7168451aec6012869
Roger Lozano <lozanofierroruu@gmail.com>
2025-11-19 22:25:40 +0100

    fix: i put and overlapbox to evaluate if the cell is accesible or not. Ignore the self collider and player collider to. Maybe it coulb be a problem when the scena has got more the one player, but i coudl how to resolve it

--- Assets/Scripts/Cells/Cell.cs
+++ Assets/Scripts/Cells/Cell.cs
@@ -7,6 +7,7 @@ namespace Assets.Scripts.Cells
     public class Cell : MonoBehaviour
     {
         // ================================================== FIELDS ==================================================
+        private Vector3 m_halfExtents = new Vector3(0.5f, 0.5f, 0.5f);
         private Vector2 m_idCell;
         private Renderer m_cellRender;
         private Color m_greenColour = new(0.4f, 0.7f, 0.2f);
@@ -27,21 +28,53 @@ namespace Assets.Scripts.Cells
         }
 
         // ================================================== PUBLIC METHODS ==================================================
+        /*
+            Change the cell color if is accesible or not
+            - Is necessary the id where player is in that moment
+            - with this is possible to calculate the distance between the player's cell and the destination's cell
+            - if the distance is 1 in axis x and y, is accesible
+            - BUT this cell must be "empty"
+            - we evalute if is empty o rnot with an overlapbox, catchiong all colliders in area
+            - if the collider is diferent about the cell collider and the player, it's not accesible
+        */
         public void ChangeColorAccesibleCells(Vector2 idPlayerCell)
         {
             float distanceX = Math.Abs(idPlayerCell.x - IDCell.x);
             float distanceY = Math.Abs(idPlayerCell.y - IDCell.y);
 
-            if (distanceX <= 2f && distanceY <= 2f)
+            if (distanceX <= 1f && distanceY <= 1f)
             {
-                m_isAccesible = true;
-                m_cellRender.material.color = m_blueColour;
+                Vector3 boxCenter = transform.position + Vector3.up * 0.5f;
+                Collider[] colliders = Physics.OverlapBox(boxCenter, m_halfExtents);
+                bool hasObstacle = false;
+
+                foreach (var collider in colliders)
+                {
+                    if (collider.gameObject != this.gameObject && !collider.CompareTag("DogPlayer"))
+                    {
+                        hasObstacle = true;
+                        break;
+                    }
+                }
+
+                if(hasObstacle)
+                {
+                    m_isAccesible = false;
+                    m_cellRender.material.color = m_greenColour;
+                }
+                else
+                {
+                    m_isAccesible = true;
+                    m_cellRender.material.color = m_blueColour;
+                }
+
             }
             else
             {
                 m_isAccesible = false;
                 m_cellRender.material.color = m_greenColour;
             }
+        
         }
 
         public void ResetAllColors()
